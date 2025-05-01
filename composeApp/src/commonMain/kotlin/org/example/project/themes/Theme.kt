1e371e1bdb458dd3c83b9d291641c909848cc15b
ankitkumar <ankitkumar5049@gmail.com>
2025-05-01 19:21:18 +0530

    Added logo and new theme

--- composeApp/src/commonMain/kotlin/org/example/project/themes/Theme.kt
+++ composeApp/src/commonMain/kotlin/org/example/project/themes/Theme.kt
@@ -1,25 +1,29 @@
 package org.example.project.themes
 
+
 import androidx.compose.material.lightColors
 import androidx.compose.ui.graphics.Color
 
-val Indigo = Color(0xFF3F51B5)
-val Orange = Color(0xFFFF9800)
-val LightGray = Color(0xFFFAFAFA)
+val BrightOrange = Color(0xFFF4511E)
+val DarkOrangeShadow = Color(0xFFBF360C)
+val SoftWhite = Color(0xFFFFFFFF)
+val LightLineGray = Color(0xFFEEEEEE)
+
+// Text and error colors
 val DeepRed = Color(0xFFD32F2F)
 val TextPrimary = Color(0xFF212121)
 val TextSecondary = Color(0xFF757575)
 
 val AppLightColors = lightColors(
-    primary = Indigo,
-    primaryVariant = Indigo,
-    secondary = Orange,
-    background = LightGray,
-    surface = Color.White,
+    primary = BrightOrange,
+    primaryVariant = DarkOrangeShadow,
+    secondary = BrightOrange,
+    background = SoftWhite,
+    surface = SoftWhite,
     error = DeepRed,
-    onPrimary = Color.White,
-    onSecondary = Color.White,
+    onPrimary = SoftWhite,
+    onSecondary = SoftWhite,
     onBackground = TextPrimary,
     onSurface = TextPrimary,
-    onError = Color.White
+    onError = SoftWhite
 )
